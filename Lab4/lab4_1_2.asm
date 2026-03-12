214bb64b1235a01082d7077161118f8fe2a164fa
somessyy <yyyXXX@outlook.com>
2026-03-12 14:25:44 +0800

    code

--- Lab4/lab4_1_2.asm
+++ Lab4/lab4_1_2.asm
@@ -4,13 +4,13 @@ code segment
 
         mov ax,0020h	;1
         mov ds,ax	;2
-	
+
         mov bx,0040h	;3
 s:      sub bl,1	;4
         mov [bx],bl	;5
-	
+
         loop s		;6
-	
+
         mov ax,4c00h	;7
         int 21h		;8
 
